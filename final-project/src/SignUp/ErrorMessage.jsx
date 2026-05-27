8376cf36dfe9a57c946a957402f0540e990638c8
bobdabuilder69 <159720034+bobdabuilder69@users.noreply.github.com>
2026-05-26 20:15:36 -0700

    Delete final-project directory

--- final-project/src/SignUp/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-// ErrorMessage.jsx
-import React from "react";
-
-export default function ErrorMessage({ errorMessage, className }) {
-  if (!errorMessage || errorMessage.length === 0) return null;
-  return <p className={className}>{errorMessage[0]}</p>;
-}
