6f07e17b99b8aad4636ab981fcbceeda707093ae
bobdabuilder69 <sidneybees@gmail.com>
2026-06-01 17:40:17 -0700

    Configure GitHub Pages deployment via Vite and GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/8th-grade-web-spring-tri/",
     plugins: [react()],
     build: {
-        outDir: "../dist",
+        outDir: "dist",
         emptyOutDir: true,
     },
     server: {
